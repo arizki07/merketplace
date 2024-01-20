1aba48d4669515e1be444878003eb75c2f453c5f
arizki07 <116191090+arizki07@users.noreply.github.com>
2024-01-20 09:00:57 +0700

    tambahanModalDetail

--- app/Views/pages/admin/list-jasa/detail.php
+++ app/Views/pages/admin/list-jasa/detail.php
@@ -16,13 +16,37 @@
                             <p class="text-muted mb-2">Category: <?= $item['jenis_jasa']; ?></p>
                             <p class="lead mb-4">Rp.<?= $item['harga_jasa']; ?></p>
                             <!-- Add to cart button -->
-                            <button class="btn btn-primary">Add to Cart</button>
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal<?= $item['id_jasa']; ?>">
+                                Add to Cart
+                            </button>
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModal<?= $item['id_jasa']; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Confirmation</h5>
+                                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            Are you sure you want to add "<?= $item['nama_jasa']; ?>" to your cart?
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                            <button type="button" class="btn btn-primary">Add to Cart</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End of Modal -->
                         </div>
                     </div>
                 <?php endforeach; ?>
 
             </div>
         </div>
+
     </div>
 </div>
 
